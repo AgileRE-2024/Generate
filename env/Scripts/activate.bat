c83ba3bcdd08cd2eda5e54ad92f4234337c20b9d
Yushan Aisyah P.I <68349067+Yushan-Riry@users.noreply.github.com>
2024-10-31 11:23:03 +0700

    ubah folder
    landing page

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\SEMESTER 5\PPL Prak\MINGGU 5\CaseDesigner\env
+set VIRTUAL_ENV=C:\Users\Dell Latitude 7280\Documents\Kuliah\Smt-5\Pembangunan Perangkat Lunak\Praktikum\project-generate\env
 
 if not defined PROMPT set PROMPT=$P$G
 
